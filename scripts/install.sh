eb7e4e424fafe5ad26a9844d3124245bb5f9b1bb
abhishektiwari90 <141727851+abhishektiwari90@users.noreply.github.com>
2025-05-27 17:04:07 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+set -e
+
+echo "Running install.sh script..."
+
+# Ensure Node.js is installed
+if ! command -v npm &> /dev/null
+then
+    echo "npm not found, installing Node.js..."
+    curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+    sudo apt install -y nodejs
+fi
+
 cd /home/ubuntu/tiwari
-chmod +x install.sh
 npm install
